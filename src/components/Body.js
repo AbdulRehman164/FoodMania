5c3c34167233dbc4d3b2363616d8c62b6c94dfc8
Abdul Rehman <abdulrehmang571@gmail.com>
2024-04-15 22:14:46 +0500

    added search bar

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,12 +1,42 @@
 import RestaurantCard from './RestaurantCard';
 import { resList } from '../constants';
+import { useState } from 'react';
+
+function handleSearch(inputText) {
+    const filteredData = resList.filter((res) =>
+        res.data.name.toLowerCase().includes(inputText.toLowerCase())
+    );
+    return filteredData;
+}
 
 const Body = () => {
+    const [inputText, setInputText] = useState('');
+    const [restaurants, setRestaurants] = useState(resList);
     return (
         <main>
-            {resList.map((res) => {
-                return <RestaurantCard {...res.data} key={res.data.id} />;
-            })}
+            <div className="search-bar">
+                <input
+                    type="text"
+                    placeholder="Search..."
+                    value={inputText}
+                    onChange={(e) => {
+                        setInputText(e.target.value);
+                    }}
+                />
+                <button
+                    onClick={() => {
+                        const filteredData = handleSearch(inputText);
+                        setRestaurants(filteredData);
+                    }}
+                >
+                    Search
+                </button>
+            </div>
+            <div className="restaurant-list">
+                {restaurants.map((res) => {
+                    return <RestaurantCard {...res.data} key={res.data.id} />;
+                })}
+            </div>
         </main>
     );
 };
